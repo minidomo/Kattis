3517d01281b6d84107712ec436d6498b784fcfb1
MiniDomo <32505118+MiniDomo@users.noreply.github.com>
2018-02-28 08:51:23 -0600

    Create test2.java

---
+++ Need to Update/test2.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class test2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<Integer> x = new ArrayList<>();
+        ArrayList<Integer> y = new ArrayList<>();
+
+        int stars = sc.nextInt();
+        System.out.println(stars + ":");
+        int
+        for (int a = 0; a < Math.round(stars/2); a++) {
+            for(int s = 0; s < )
+        }
+
+        sc.close();
+    }
+}
