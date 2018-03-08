3c57ef014d9487a7c188f219df2079d1cab7f0ee
MiniDomo <32505118+MiniDomo@users.noreply.github.com>
2018-03-08 08:50:31 -0600

    Create format.java

---
+++ Need to Update/format.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class format {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int cases = sc.nextInt();
+        while(cases-- > 0) {
+            int caseNum = sc.nextInt();
+            System.out.println(caseNum);
+            int num = sc.nextInt();
+
+        }
+
+        sc.close();
+    }
+}
