ec8f85a2c86155eec3603b07efc1198ac7054990
MiniDomo <32505118+MiniDomo@users.noreply.github.com>
2018-03-11 08:31:41 -0600

    Update ostgotska.java

--- Java/ostgotska.java
+++ Java/ostgotska.java
@@ -1,14 +1,12 @@
 
 // https://open.kattis.com/problems/ostgotska
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class ostgotska {
-    public static void main(String[] args) throws IOException {
-        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        String[] words = sc.readLine().split(" ");
+        String[] words = sc.nextLine().split(" ");
         double count = 0;
         for (String s : words)
             if (s.contains("ae"))
