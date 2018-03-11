93dbd4b71e535e726304d013bfa6d59816d3aace
MiniDomo <32505118+MiniDomo@users.noreply.github.com>
2018-03-11 01:36:14 -0600

    Update ostgotska.java

--- Java/ostgotska.java
+++ Java/ostgotska.java
@@ -3,7 +3,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class ostgotska {
     public static void main(String[] args) throws IOException {
