5bbef54523dc7491df459bf4d33fcd2c043e692a
MiniDomo <32505118+MiniDomo@users.noreply.github.com>
2018-03-11 01:36:02 -0600

    Update shatteredcake.java

--- Java/shatteredcake.java
+++ Java/shatteredcake.java
@@ -3,7 +3,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class shatteredcake {
     public static void main(String[] args) throws IOException {
