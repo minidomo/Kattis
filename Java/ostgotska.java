5ee563f480f90a0d86d02dab6a7d55eca7f6422f
MiniDomo <32505118+MiniDomo@users.noreply.github.com>
2018-03-11 01:33:24 -0600

    Add files via upload

---
+++ Java/ostgotska.java
@@ -0,0 +1,20 @@
+
+// https://open.kattis.com/problems/ostgotska
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class ostgotska {
+    public static void main(String[] args) throws IOException {
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] words = sc.readLine().split(" ");
+        double count = 0;
+        for (String s : words)
+            if (s.contains("ae"))
+                count++;
+        System.out.println(count / words.length >= .4 ? "dae ae ju traeligt va" : "haer talar vi rikssvenska");
+        sc.close();
+    }
+}
