e69ea51799ce32d26ef0eceae9f7ae37e0667d7a
MiniDomo <32505118+MiniDomo@users.noreply.github.com>
2018-03-11 08:33:13 -0600

    Update shatteredcake.java

--- Java/shatteredcake.java
+++ Java/shatteredcake.java
@@ -1,20 +1,16 @@
 
 // https://open.kattis.com/problems/shatteredcake
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class shatteredcake {
-    public static void main(String[] args) throws IOException {
-        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int width = Integer.parseInt(sc.readLine());
-        int pieces = Integer.parseInt(sc.readLine());
+        int width = sc.nextInt();
+        int pieces = sc.nextInt();
         int area = 0;
         while (pieces-- > 0) {
-            String w = sc.readLine();
-            int s = w.indexOf(' ');
-            area += Integer.parseInt(w.substring(0, s)) * Integer.parseInt(w.substring(s + 1));
+            area += sc.nextInt() * sc.nextInt();
         }
         System.out.println(area / width);
 
