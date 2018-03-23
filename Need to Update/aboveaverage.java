754d1025313f24f948bfb8467ceec262b847aa6e
MiniDomo <32505118+MiniDomo@users.noreply.github.com>
2018-03-23 13:42:33 -0500

    Create aboveaverage.java

---
+++ Need to Update/aboveaverage.java
@@ -0,0 +1,29 @@
+
+// https://open.kattis.com/problems/aboveaverage
+import java.util.Scanner;
+
+public class aboveaverage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int cases = sc.nextInt();
+        while (cases-- > 0) {
+            double students = sc.nextDouble();
+            int[] grades = new int[(int) students];
+            int i = 0;
+            double sum = 0;
+            for (int x = 0; x < students; x++) {
+                int person = sc.nextInt();
+                sum += person;
+                grades[i++] = person;
+            }
+            sum /= students;
+            double count = 0;
+            for (int s : grades)
+                if (s > sum)
+                    count++;
+            System.out.printf("%.3f%s", ((count / students) * 100), "%\n");
+        }
+        sc.close();
+    }
+}
