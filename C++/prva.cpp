cecaa05415f5e3e6ff4814797bf4c7ecda022ca2
MiniDomo <32505118+MiniDomo@users.noreply.github.com>
2018-05-30 18:20:33 -0500

    Solved prva

---
+++ C++/prva.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cstddef>
+
+using namespace std;
+
+vector<string> words;
+
+void getWords(string s)
+{
+    size_t i = s.find_first_of('#');
+    size_t start = 0;
+    if (i != string::npos)
+    {
+        while (i != string::npos)
+        {
+            string temp = s.substr(start, i - start);
+            if (temp.size() > 1)
+                words.push_back(temp);
+            start = i + 1;
+            i = s.find_first_of('#', start);
+        }
+        if (s.size() - start > 1)
+            words.push_back(s.substr(start));
+    }
+    else
+        words.push_back(s);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    int rows, cols;
+    cin >> rows >> cols;
+    vector<string> arr;
+    for (int r = 0; r < rows; r++)
+    {
+        string s;
+        cin >> s;
+        arr.push_back(s);
+        getWords(s);
+    }
+
+    for (int c = 0; c < cols; c++)
+    {
+        string s = "";
+        for (int r = 0; r < rows; r++)
+            s += arr[r][c];
+        getWords(s);
+    }
+
+    sort(words.begin(), words.end());
+
+    cout << words[0];
+    return 0;
+}
