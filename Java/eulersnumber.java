98375c1a5c92db7b84bda59993c263e14604c18e
JB Ladera <32505118+MiniDomo@users.noreply.github.com>
2018-11-04 01:37:56 -0600

    removed redundancy

--- Java/eulersnumber.java
+++ Java/eulersnumber.java
@@ -7,14 +7,10 @@ public class eulersnumber {
         BufferedWriter dc = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(sc.readLine());
-        if (n == 0) {
-            dc.write("1");
-        } else {
-            double sum = 1, prev = 1;
-            for (int x = 1; x <= n; x++)
-                sum += prev *= 1d / x;
-            dc.write(sum + "\n");
-        }
+        double sum = 1, prev = 1;
+        for (int x = 1; x <= n; x++)
+            sum += prev *= 1d / x;
+        dc.write(sum + "\n");
 
         dc.close();
         sc.close();
