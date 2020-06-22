8032e2379693c72ff68bf5cdb298d68a0921e282
JB Ladera <32505118+MiniDomo@users.noreply.github.com>
2020-06-21 21:37:16 -0500

    fixed formatting

--- C++/gcpc.cpp
+++ C++/gcpc.cpp
@@ -1,12 +1,15 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <ext/pb_ds/assoc_container.hpp>
 
 using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
 using namespace __gnu_pbds;
 
-template <class T> using Tree = tree<T, null_type, less<T>, 
-    rb_tree_tag, tree_order_statistics_node_update>; 
+template <class T>
+using Tree =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 struct A {
     int t, s, p;
